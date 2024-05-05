85ab1125a86b49d95bb63c89d23f195c63f3f1fd
chujiu <letchujiufly@outlook.com>
2024-05-05 16:35:14 +0800

    Nested Routes and index Route

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/app" element={<AppLayout />} />
-        <Route path="/login" element={<Login />} />
+        <Route index element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>Welcome to the app</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Country</p>} />
+          <Route path="from" element={<p>From</p>} />
+        </Route>
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
@@ -7,7 +8,7 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>lot of cities</p>
+      <Outlet />
       <footer className={styles.footer}>© Copying 2024 Worldwish Inc.</footer>
     </div>
   );
